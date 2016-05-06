b2f5301844524744c2cca45a6ec303c60de5c551
Abhishek Gautam <agautam2@buffalo.edu>
2016-05-05 20:10:39 -0400

    First Commit

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/DBHandler.java
@@ -0,0 +1,36 @@
+package edu.buffalo.cse.cse486586.simpledynamo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Gautam on 5/5/16.
+ */
+public class DBHandler extends SQLiteOpenHelper
+{
+    private static  String dbName="mainTuple";
+    private static int version=1;
+
+    private static final String schema = "CREATE TABLE dhtRecords (key DATA, value DATA, association DATA)";
+
+    public DBHandler(Context context)
+    {
+        super(context, dbName, null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL(schema);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int obsolete, int latest)
+    {
+        //logic understood from - http://stackoverflow.com/questions/3675032/drop-existing-table-in-sqlite-when-if-exists-operator-is-not-supported
+        db.execSQL("DROP TABLE IF EXISTS dhtRecords");
+        onCreate(db);
+    }
+
+}
---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -0,0 +1,90 @@
+package edu.buffalo.cse.cse486586.simpledynamo;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.concurrent.ExecutionException;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+public class SimpleDynamoProvider extends ContentProvider
+{
+
+	@Override
+	public int delete(Uri uri, String selection, String[] selectionArgs)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri uri)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+    private String genHash(String input) throws NoSuchAlgorithmException
+	{
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        byte[] sha1Hash = sha1.digest(input.getBytes());
+        Formatter formatter = new Formatter();
+        for (byte b : sha1Hash) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
+    }
+}
